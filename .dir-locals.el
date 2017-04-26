e12913021068565f310b1cfa97d3797b84066f2a
Ben Hutchings <ben@decadent.org.uk>
2017-04-26 02:00:05 +0100

    Imported Debian version 0.130

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((sh-mode
+  (sh-basic-offset . 8)
+  (sh-indentation . 8)
+  (sh-indent-for-case-label . 0)
+  (sh-indent-for-case-alt . +)))
